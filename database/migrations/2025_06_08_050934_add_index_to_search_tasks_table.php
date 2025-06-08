69eb4e90c8d5f68fd22eba290e73930aaeba5050
Kaio Da Dalt <kaiodadalt@gmail.com>
2025-06-08 02:14:15 -0300

    feature: add index to search the Tasks table

---
+++ database/migrations/2025_06_08_050934_add_index_to_search_tasks_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->index(['created_by', 'assigned_to', 'status', 'priority', 'created_at'], 'tasks_search_idx');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropIndex('tasks_search_idx');
+        });
+    }
+};
